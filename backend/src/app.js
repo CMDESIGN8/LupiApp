a8addf27f90e3c0d59dd2234524c93823b46f50f
CMDESIGN8 <cm@lowcargo.lat>
2025-08-16 18:18:41 -0300

    sad

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,34 +1,33 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-require("dotenv").config();
+  const express = require("express");
+  const cors = require("cors");
+  const bodyParser = require("body-parser");
+  require("dotenv").config();
 
-const authRoutes = require("./routes/auth");
-const missionsRoutes = require("./routes/missions");
-const cardsRoutes = require("./routes/cards");
-const clubsRoutes = require("./routes/clubs");
-const prizesRoutes = require("./routes/prizes");
-const userRoutes = require('./routes/userRoutes'); 
+  const authRoutes = require("./routes/auth");
+  const missionsRoutes = require("./routes/missions");
+  const cardsRoutes = require("./routes/cards");
+  const clubsRoutes = require("./routes/clubs");
+  const prizesRoutes = require("./routes/prizes");
+  const userRoutes = require('./routes/userRoutes'); 
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+  const app = express();
+  const PORT = process.env.PORT || 5000;
 
-// Middlewares
-app.use(cors());
-app.use(bodyParser.json());
+  // Middlewares
+  app.use(cors());
+  app.use(bodyParser.json());
 
-// Rutas
-app.use("/api/auth", authRoutes);
-app.use("/api/missions", missionsRoutes);
-app.use("/api/cards", cardsRoutes);
-app.use("/api/clubs", clubsRoutes);
-app.use("/api/prizes", prizesRoutes);
-app.use("/api/auth", authRoutes);
-app.use('/api/users', userRoutes);
+  // Rutas
+  app.use("/api/auth", authRoutes);
+  app.use("/api/missions", missionsRoutes);
+  app.use("/api/cards", cardsRoutes);
+  app.use("/api/clubs", clubsRoutes);
+  app.use("/api/prizes", prizesRoutes);
+  app.use('/api/users', userRoutes);
 
-// Ruta de prueba
-app.get("/", (req, res) => {
-  res.send("Backend de Lupi Futbol RPG funcionando!");
-});
+  // Ruta de prueba
+  app.get("/", (req, res) => {
+    res.send("Backend de Lupi Futbol RPG funcionando!");
+  });
 
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+  app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
