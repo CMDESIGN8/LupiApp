f83b632c8f38b352a3472a1686fa0e4e840d53b8
CMDESIGN8 <cm@lowcargo.lat>
2025-08-17 15:31:53 -0300

    sdasd

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,7 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'es2020', // <--- ¡Asegúrate de que sea 'es2020' o superior!
-    outDir: 'dist',
-    // ... otras opciones de build
-  },
-});
+})
